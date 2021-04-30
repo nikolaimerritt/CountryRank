fc7460efdaa47781db7a57f92f3690162b91610c
Nikolai Merritt <nikolaimerritt20junk@gmail.com>
2021-04-30 17:25:27 +0100

    fetches country data from flight api and iata code lookup table from website
    saves country data and iata lookup table to json files to reduce web requests

--- fetchDataFromAPI.py
+++ fetchDataFromAPI.py
@@ -1,53 +1,67 @@
 import requests
 import json
+from bs4 import BeautifulSoup
+
+def dumpJson(file, Object):
+    """ dumps the json of a json-able object `Object` to a file `file"""
+    content = json.dumps(Object)
+    with open(file, "w") as f:
+        f.write(content)
+
 
 def readAccessToken():
+    """ reads access token from file """
     with open("AccessToken.txt", "r") as f:
         return f.read()
 
-def result(domain, extraParams):
-    """
-    returns a list of results from `domain` with extra params `extraParams`
+
+def getDomainData(domain, totalEntries, extraParams = {}):
+    """ 
+    returns list of data from "domain" (e.g. "flights", "countries"), 
     
-    e.g. `domain = "countries"`, `extraParams = {"limit": 10}`
+    with optional extra parameters (e.g. `dep_iata = XYZ`) 
+
+    from aviationstack api. list is <= `totalEntries` in length, and `ceil(totalEntries/100)` calls are made
     """
-    url = "http://api.aviationstack.com/v1/{0}".format(domain)
-    params = { "access_key": readAccessToken() }
-    params.update(extraParams)
-    response = requests.get(url, params)
-    return response.json()["data"]
-
-
-def getCountryData():
-    countryData = []
-    totalCountries = 252
-    stepSize = 100
-    for offset in range(0, totalCountries, stepSize):
-        countryData += result(
-            "countries", {
-                "offset": offset,
-                "limit": stepSize
-            }
-        )
-    return countryData
-
-
-def getDomainData(domain, totalEntries):
     data = []
-    stepSize = 100
-    for offset in range(0, totalEntries, stepSize):
-        data += result(
-            domain, 
-            {
-                "offset": offset,
-                "limit": stepSize
-            }
-        )
+    entriesPerCall = 100
+    url = "http://api.aviationstack.com/v1/{0}".format(domain)
+    
+    params = { 
+        "access_key": readAccessToken(),
+        "offset": totalEntries,
+        "limit": entriesPerCall, 
+    }
+    params.update(extraParams)
+    
+    for offset in range(0, totalEntries, entriesPerCall):
+        response = requests.get(url, params)
+        data += response.json()["data"]
     return data
 
 
-with open("cityData.json", "w") as f:
-    f.write(str(getDomainData("cities", 500)))
 
-with open("countryData.json", "w") as f:
-    f.write(str(getDomainData("countries", 200)))
+def getIataCodesOfCities():
+    """
+    scrapes website with lookup table of cities and their IATA codes
+
+    returns `{city_name: iata_code}` dict
+    """
+    html = requests.get("https://www.nationsonline.org/oneworld/IATA_Codes/airport_code_list.htm").content
+    soup = BeautifulSoup(html, "html.parser")
+    cityToIata = {}
+    
+    for row in soup.find_all("tr"):
+        cells = row.find_all("td")
+        if len(cells) >= 3:
+            cityToIata.update({cells[0].get_text(): cells[2].get_text()})
+    return cityToIata
+
+
+def getFlightDataFromCity(cityIataCode, numResults):
+    return getDomainData("flights", numResults, extraParams={"dep_iata": cityIataCode})
+
+
+def dumpStaticData():
+    dumpJson(getDomainData("countries", 200))
+    dumpJson("iataCodes.json", getIataCodesOfCities())
